3f9bf266390ddf443dbb5bfe23f5953b5a510f89
Jordan Patel <21124677+JordansGit@users.noreply.github.com>
2023-06-14 07:43:23 +0100

    feat: image slider

--- js/script.js
+++ js/script.js
@@ -4,6 +4,8 @@ let navList = document.querySelector('.nav-list');
 let header = document.querySelector('.header');
 let nav = document.querySelector('.nav');
 
+// Mobile Menu 
+
 mobileMenu.addEventListener('click', function() {
   header.classList.add('nav-open-header');
   nav.classList.add('nav-open-nav');
@@ -19,3 +21,43 @@ navCloseBtn.addEventListener('click', function() {
   navList.classList.remove('nav-open-nav-list');
   document.body.classList.remove('nav-open-body');
 })
+
+// Image slider 
+
+let heroImagesDesktop = document.getElementsByClassName("hero-img-desktop");
+let heroImagesMobile = document.getElementsByClassName("hero-img-mobile");
+
+let slideIndex = 1;
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function showSlides(n) {
+  let slideImages;
+  let slideText = document.getElementsByClassName("slide-text");
+  if(window.matchMedia("(min-width: 800px)").matches) {
+    slideImages = heroImagesDesktop;
+  } else {
+    slideImages = heroImagesMobile;
+  }
+
+  if (n > slideImages.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slideImages.length}
+
+  for (let i = 0; i < slideImages.length; i++) {
+    slideImages[i].style.display = "none"; 
+  }
+  for (let i = 0; i < slideText.length; i++) {
+    slideText[i].style.display = "none"; 
+  }
+  slideImages[slideIndex-1].style.display = "block";  
+  slideText[slideIndex-1].style.display = "block";  
+}
+
+/* Notes / To do: 
+
+It messes up when I resize from mobile to desktop. 
+I have to refresh the page in order to get it working properly again. 
+
+*/ 
